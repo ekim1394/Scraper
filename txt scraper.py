7266f49067bc6211a9ce1770d1d9ec0a6889bc26
Eugene Kim <simplyeugene94@gmail.com>
2016-07-21 17:16:10 -0400

    Shelf and update
    Created shelf to cp files

--- txt scraper.py
+++ txt scraper.py
@@ -4,29 +4,44 @@ by reading from the work folder
 and writing new files into the done folder
 
 It will separate data files into lists and organize them accordingly
-'''
-import sys, os, glob
 
-#variables needed
+glob is used to find specific file types
+shelve is used to save variables to copy onto final txts
+pyperclip is used to copy/paste
+shutil is used to move/modify files from folder to folder
+'''
+import sys, os, glob, shelve, pyperclip, shutil
+
+# variables needed
 directory = 'C:\Users\e.a.kim\Documents\TXT Scraper'
 work = '\work'
 done = '\done'
-
-#sets working directory to work directory
+wpShelf = shelve.open('wp')
+labels = []
+# sets working directory to work directory
 os.chdir(directory+work)
 
-#manifest files
+#read manifest files
 for filename in glob.glob('*.manifest'):
-   print filename #returns all files in work folder with
+   workFile = directory+work+'\\'+filename
+   openFile = open(workFile, 'r') #opens file for editing
+   readFile = openFile.readlines() #opens file line by line in list
 
-#med files
-for filename in glob.glob('*.med'):
-   print filename #returns all files in work folder with
+# lists each line of file skipping line breaks
+for line in readFile:
+   if line == '\\n':
+       continue
+   else:
+       label_id = line.split('|')[1]
+       print label_id
+       labels.append(label_id)
 
-#
-for filename in glob.glob('CDAS_POS_ACP*'):
-   print filename #returns all files in work folder with
+print labels
 
 
-#sets working directory to done
+
+# sets working directory to done
 os.chdir(directory+done)
+
+# close shelf
+wpShelf.close()
